99aa94a747f82dc91acab4ac862370cd198afa85
ArtemSermyagin <artem.sermyagin@yahoo.com>
2024-07-23 09:55:27 +0400

    исправление моделей и админки редактировал settings.py

--- network/admin.py
+++ network/admin.py
@@ -1,24 +1,31 @@
 from django.contrib import admin
-from .models import Network, Contacts, Products
+from .models import Network, Contact, Product
 
-class ContactsInline(admin.TabularInline):
-    model = Contacts
 
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 1
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 1
+
+
+@admin.register(Network)
 class NetworkAdmin(admin.ModelAdmin):
-    list_display = ('name', 'provider', 'liability', 'time_creation')
-    list_filter = ('city',)
-    inlines = [ContactsInline]
+    list_display = ('id', 'name', 'network', 'liability', 'created_at')
+    inlines = [ProductInline, ContactInline]
 
-    def clear_liability(modeladmin, request, queryset):
-        queryset.update(liability=0)
-    clear_liability.short_description = "Очистить задолженность"
+    # def clear_liability(modeladmin, request, queryset):
+    #     queryset.update(liability=0)
+    #
+    # clear_liability.short_description = "Очистить задолженность"
 
-admin.site.register(Network, NetworkAdmin)
 
-@admin.register(Contacts)
-class ContactsAdmin(admin.ModelAdmin):
-    list_display = ('network', 'country', 'email')
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'country', 'city', 'street', 'house_number', 'network')
 
-@admin.register(Products)
-class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'provider', 'model')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'model', 'launch_date', 'network')
--- network/models.py
+++ network/models.py
@@ -3,32 +3,38 @@ from django.db import models
 
 class Network(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
-    country = models.CharField(max_length=50, verbose_name='Страна')
-    provider = models.ForeignKey('self', on_delete=models.CASCADE, related_name='child_networks')
+    network = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='child_networks',
+        null=True,
+        blank=True,
+        verbose_name='Поставщик'
+    )
     liability = models.FloatField(verbose_name='Задолженность')
-    time_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     def __str__(self):
-        return f"{self.name} {self.provider} {self.liability} {self.time_creation}"
+        return f"{self.name} {self.liability} {self.created_at}"
 
 
-class Contacts(models.Model):
-    email = models.EmailField()
+class Contact(models.Model):
+    email = models.EmailField(verbose_name='Почта')
     country = models.CharField(max_length=50, verbose_name='Страна')
     city = models.CharField(max_length=50, verbose_name='Город')
-    street = models.CharField(max_length=100, verbose_name='Город')
+    street = models.CharField(max_length=100, verbose_name='Улица')
     house_number = models.CharField(max_length=10, verbose_name='Номер дома')
-    network = models.ForeignKey(Network, on_delete=models.CASCADE, verbose_name='Сеть')
+    network = models.ForeignKey(Network, on_delete=models.CASCADE, related_name='contacts', verbose_name='Поставщик')
 
     def __str__(self):
-        return f"{self.network} {self.country} {self.email}"
+        return f"{self.country} {self.city} {self.street} {self.network.id}"
 
-class Products(models.Model):
+
+class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     model = models.CharField(max_length=200, verbose_name='Модель')
-    product_launch_date = models.DateTimeField(verbose_name='Дата запуска продукта')
-    provider = models.ForeignKey(Network, on_delete=models.CASCADE, related_name='child_networks')
+    launch_date = models.DateTimeField(verbose_name='Дата запуска продукта')
+    network = models.ForeignKey(Network, on_delete=models.CASCADE, related_name='products', verbose_name='Поставщик')
 
     def __str__(self):
-        return f"{self.name} {self.provider} {self.model}"
-
+        return f"{self.name} {self.model} {self.network.id}"
